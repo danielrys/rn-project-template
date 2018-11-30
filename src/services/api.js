1c922a456d7f0c2e8469948a6276ce98af96e7b0
Daniel Rys <danielrys@gmail.com>
2018-11-30 11:36:07 +0100

    Create config file

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 // @flow
 import axiosLib from "axios"
+import config from "../config/appConfig"
 
 export const axios = axiosLib.create({
-  baseURL: "@TODO",
+  baseURL: config.baseUrl,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
